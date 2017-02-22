bd44bb1c71083f05babfdcc36f55c535345b5dd8
Richard G Schmidt Jr <richard.g.schmidt.jr@gmail.com>
2017-02-22 00:35:14 -0600

    Starting Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule} from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
 
 import { AppComponent }  from './app.component';
 
@@ -11,8 +13,12 @@ import { AppComponent }  from './app.component';
                   FormsModule,
                 ],
   declarations: [ AppComponent,
-                  HeroDetailComponent
+                  HeroDetailComponent,
+                  HeroesComponent
                    ],
+  providers: [
+                  HeroService
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
