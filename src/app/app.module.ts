8012bff03d5697387d2e546823a78fd3841cf112
Richard G Schmidt Jr <richard.g.schmidt.jr@gmail.com>
2017-02-22 02:00:00 -0600

    Mid routing and css / html cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,37 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule} from '@angular/forms';
+
+import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
 
-import { AppComponent }  from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
-@NgModule({
+
+
+
+
+@NgModule
+({
   imports:      [   
                   BrowserModule,
                   FormsModule,
+                  AppRoutingModule,
                 ],
   declarations: [ AppComponent,
                   HeroDetailComponent,
-                  HeroesComponent
-                   ],
-  providers: [
+                  HeroesComponent,
+                  DashboardComponent
+                ],
+  providers:    [
                   HeroService
-  ],
-  bootstrap:    [ AppComponent ]
+                ],
+  bootstrap:    [ 
+                  AppComponent 
+                ]
 })
 export class AppModule { }
